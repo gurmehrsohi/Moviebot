d989e0a057112ebaf44f57ab3cc22af476ad2f30
Gurmehr Sohi <gurusohi@Gurmehrs-MacBook-Pro.com>
2016-08-28 14:56:03 +0530

    final

--- app/views.py
+++ app/views.py
@@ -81,8 +81,9 @@ def new_movie(fbid,recevied_message):
                         }
             }
 
-   ''' response_message = json.dumps({"recipient":{"id":fbid},"message":message_object})
-    status = requests.post(post_message_url,headers={"Content-Type": "application/json"},data=response_message)'''
+#response_message = json.dumps({"recipient":{"id":fbid},"message":message_object})
+#status = requests.post(post_message_url,headers={"Content-Type": "application/json"},data=response_message)
+        
     response_message2 = json.dumps({"recipient":{"id":fbid},"message":message_object2})
     status = requests.post(post_message_url,headers={"Content-Type": "application/json"},data=response_message2)
     pprint(status.json())
